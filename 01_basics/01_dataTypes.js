6553b9124c122f1fd50acd60408c17649c2fad9e
ashutosh12-code <ashutoshparmar567@gmail.com>
2026-02-17 16:19:50 +0000

    container config done

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // it treats all the js code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+//readability of the code should be high 
+
+let name="ashutosh"
+let age = 21
+let isLoggedIn = true
+
+/*
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+*/
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 23142
+let accountEmail = "ashutosh@gmail.com"
+var accountPassword = 12345
+accountCity = "Jaipur"
+
+//accountId=34 not allowed
+//var is not prefered in the modern world let is used
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
